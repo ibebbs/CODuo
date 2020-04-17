2a7f625787bc03fc15e5d7a8e3159c3d31a24e07
ibebbs <github@bebbs.co.uk>
2020-04-17 14:20:18 +0100

    Wasm project brought up to date but failing to build due to: "Mono.Cecil.ResolutionException: Failed to resolve !!0& System.Runtime.CompilerServices.Unsafe::Unbox"

---
+++ src/CODuo/CODuo.Wasm/Platform/Schedulers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading;
+
+namespace CODuo.Platform
+{
+    public class Schedulers : ISchedulers
+    {
+        private static readonly Lazy<IScheduler> DispatchScheduler = new Lazy<IScheduler>(() => new SynchronizationContextScheduler(SynchronizationContext.Current));
+
+        public IScheduler Default => Scheduler.Default;
+
+        public IScheduler Dispatcher => DispatchScheduler.Value;
+    }
+}
---
+++ src/CODuo/CODuo.Wasm/Platform/Services.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CODuo.Platform
+{
+    public partial class Services
+    {
+        partial void RegisterPlatformServices(IServiceCollection services)
+        {
+            services.AddSingleton<ISchedulers, Schedulers>();
+        }
+    }
+}
