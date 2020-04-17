b52509bf9c16a62d59acbd1fb339a50240689fef
ibebbs <github@bebbs.co.uk>
2020-04-17 11:16:40 +0100

    Reverted to use TwoPaneView as it functions better with iOS. Still issues with Surface Duo (uk map scrolls) but will leave as is for now.

--- src/CODuo/CODuo.Shared/Root/Helpers.cs
+++ src/CODuo/CODuo.Shared/Root/Helpers.cs
@@ -1,4 +1,5 @@
 ﻿using CODuo.Controls;
+using Microsoft.UI.Xaml.Controls;
 using System;
 
 namespace CODuo.Root
@@ -12,6 +13,17 @@ namespace CODuo.Root
                 DualPaneViewMode.SinglePane => Platform.Layout.Mode.Single,
                 DualPaneViewMode.Tall => Platform.Layout.Mode.TopBottom,
                 DualPaneViewMode.Wide => Platform.Layout.Mode.LeftRight,
+                _ => throw new ArgumentException($"Unknown DualPaneViewMode: '{mode}'", nameof(mode))
+            };
+        }
+
+        public static Platform.Layout.Mode MapMode(TwoPaneViewMode mode)
+        {
+            return mode switch
+            {
+                TwoPaneViewMode.SinglePane => Platform.Layout.Mode.Single,
+                TwoPaneViewMode.Tall => Platform.Layout.Mode.TopBottom,
+                TwoPaneViewMode.Wide => Platform.Layout.Mode.LeftRight,
                 _ => throw new ArgumentException($"Unknown DualPaneViewMode: '{mode}'", nameof(mode))
             };
         }
--- src/CODuo/CODuo.Shared/Root/View.xaml.cs
+++ src/CODuo/CODuo.Shared/Root/View.xaml.cs
@@ -3,16 +3,9 @@ using System;
 using System.Reactive.Linq;
 using Windows.Foundation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using System.Reactive;
 using Windows.UI.Xaml.Input;
 using CODuo.Controls;
-#if NETFX_CORE
-using TwoPaneView = Windows.UI.Xaml.Controls.TwoPaneView;
-using NavigationView = Windows.UI.Xaml.Controls.NavigationView;
-using NavigationViewItem = Windows.UI.Xaml.Controls.NavigationViewItem;
-using NavigationViewItemInvokedEventArgs = Windows.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs;
-#endif
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -21,19 +14,19 @@ namespace CODuo.Root
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class View : Page, IView
+    public sealed partial class View : Windows.UI.Xaml.Controls.Page, IView
     {
         public View()
         {
             this.InitializeComponent();
 
             CurrentMode = Observable
-                .FromEvent<TypedEventHandler<DualPaneView, object>, DualPaneView>(
+                .FromEvent<TypedEventHandler<TwoPaneView, object>, TwoPaneView>(
                     action => (s, e) => action(s),
-                    handler => dualPaneView.ModeChanged += handler,
-                    handler => dualPaneView.ModeChanged -= handler)
+                    handler => twoPaneView.ModeChanged += handler,
+                    handler => twoPaneView.ModeChanged -= handler)
                 .Select(twoPaneView => twoPaneView.Mode)
-                .StartWith(dualPaneView.Mode)
+                .StartWith(twoPaneView.Mode)
                 .Select(mode => Helpers.MapMode(mode));
 
             RefreshData = Observable
@@ -46,8 +39,8 @@ namespace CODuo.Root
 
         public void PerformLayout(Layout layout)
         {
-            dualPaneView.Pane1 = layout.Pane1Content as UIElement;
-            dualPaneView.Pane2 = layout.Pane2Content as UIElement;
+            twoPaneView.Pane1 = layout.Pane1Content as UIElement;
+            twoPaneView.Pane2 = layout.Pane2Content as UIElement;
         }
 
         public IObservable<Platform.Layout.Mode> CurrentMode { get; }
